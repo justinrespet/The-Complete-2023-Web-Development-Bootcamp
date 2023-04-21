26f8792785c93130deedfd594339b397ac02db0a
Justin Respet <justinrespet@gmail.com>
2023-04-21 07:50:23 -0600

    Step 6 of Simon Game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,7 +1,54 @@
 
+// variables to contain game and user sequences
+var gamePattern = []
+var userClickedPattern = []
 
+// variable to indicate if user has pressed key
+var gameBegunFlag = false
+
+
+var buttonColours = ["red", "blue", "green", "yellow"]
 
 function nextSequence(){
     var randomNumber =  Math.floor( Math.random() * 4 )
-    return randomNumber
+    return buttonColours[randomNumber]
 }
+
+gamePattern.push(nextSequence())
+
+// Detect keypress to begin game
+$(document).keydown(function(event){
+
+    if(!gameBegunFlag)
+    {
+        simonSays()
+        gameBegunFlag = true
+    }
+    
+    
+})
+
+// event handler to record user actions
+$(".btn").click(function() {
+    userClickedPattern.push(this.id)
+    $("#"+this.id).fadeOut(50).fadeIn(50)
+    playSound(this.id)
+})
+
+// Display pattern to follow
+function simonSays(){
+
+    for (let index = 0; index < gamePattern.length; index++) {
+    
+        playSound(gamePattern[index])
+        $("#"+gamePattern[index]).fadeOut(50).fadeIn(50)
+    
+    }
+}
+
+function playSound(colour){
+
+    var colourSound = new Audio("sounds/"+colour+".mp3")
+    colourSound.play()
+}
+
