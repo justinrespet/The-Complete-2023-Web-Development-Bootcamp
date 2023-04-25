dfdd7c1d339e430af326ccd861de90f9e4f86475
Justin Respet <justinrespet@gmail.com>
2023-04-25 07:48:47 -0600

    Using body-parser

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+    console.log(req.body.num1)
+    var answer = Number(req.body.num1) + Number(req.body.num2);
+    res.send(answer.toString())
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
