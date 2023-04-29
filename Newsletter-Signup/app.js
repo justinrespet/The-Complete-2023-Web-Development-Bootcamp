54c7cb27075b073b2ba2393cdf25174aef1e1a04
Justin Respet <justinrespet@gmail.com>
2023-04-28 19:34:23 -0600

    Inlcuded mailchimp keys

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(){
@@ -13,8 +12,25 @@ app.listen(3000, function(){
     
 })
 
+// mailchimp APIkey
+    // 0c0754c0c70e1219f6431ed51c6407f1-us17
+
+    // mailchimp Audience ID
+    // 18fb9e9a8b
+
 
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", (req, res) => {
+    
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    
+
+
 })
