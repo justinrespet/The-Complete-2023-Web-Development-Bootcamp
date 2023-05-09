c9cc89c9c5400c8745b66ae514f5d2f066dce82b
Justin Respet <justinrespet@gmail.com>
2023-05-09 12:35:32 -0600

    Import css for todo project

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,18 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var items = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/", function(req, res) {
     
     var today = new Date();
     var currentDay = today.getDay();
+    // var day = "";
 
-    if (currentDay === 6  || currentDay === 0)
-    {
-        res.send("<h1>Yay it's the weekend</h1>");
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month: "long"
     }
-    else {
-        res.send("I must toil");
-    }
+
+    var day = today.toLocaleDateString("en-US", options)
+
+    // if (currentDay === 6  || currentDay === 0)
+    // {
+    //     day = "Weekend";
+    // }
+    // else {
+    //     day = "Weekday";
+    // }
+
+    res.render('list', {kindOfDay: day, newListItem: items});
+})
+
+app.post("/", (req, res) => {
+    items.push(req.body.newItem)
+    
+    res.redirect("/")
 })
 
 app.listen(3000, function() {
