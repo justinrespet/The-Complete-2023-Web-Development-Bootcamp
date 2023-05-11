a2d301a4868cb9c0cba94090f3643e8ba56c16dd
Justin Respet <justinrespet@gmail.com>
2023-05-11 08:38:06 -0600

    Add header and footer templates with EJS

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,9 +5,11 @@ const app = express();
 
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var items = [];
+let workItems = []
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
     
@@ -31,13 +33,34 @@ app.get("/", function(req, res) {
     //     day = "Weekday";
     // }
 
-    res.render('list', {kindOfDay: day, newListItem: items});
+    res.render('list', {listTitle: day, newListItem: items});
 })
 
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work List", newListItem: workItems})
+})
+
+app.post("/work", (req, res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
 app.post("/", (req, res) => {
-    items.push(req.body.newItem)
+
+    let item = req.body.newItem
+
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+
     
-    res.redirect("/")
+    
 })
 
 app.listen(3000, function() {
